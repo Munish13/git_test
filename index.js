e28a1e740870087df0b524fb527796f7e08be0e5
Munish <munishkumar.web@gmail.com>
2025-04-24 12:27:35 +0530

    index file updated

---
+++ index.js
@@ -0,0 +1,24 @@
+// var total = 0, count = 1;
+// while (count <= 10) {
+//     total += count;
+//     count += 1;
+// }
+// console.log(total);
+
+// function fac(n) {
+//     if (n == 0)
+//         return 1;
+//     else
+//         return fac(n - 1) * n;
+// }
+// console.log(fac(7));
+
+// function fac(n) {
+//     return (n - 1) * n;
+// }
+
+// console.log(fac(5))
+
+if (true || false) {
+    console.log("enter in if conditon")
+}
